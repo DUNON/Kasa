58d3a00a4b91d4a2ffec60cd1e31af7317a9ecf3
unknown <d.news@hotmail.fr>
2022-11-22 00:42:35 +0100

    created header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import {Link} from "react-router-dom"
+import logo from '../assets/img/LOGO.png'
+import './Header.css'
+
+export default function Header() {
+  return (
+       <header className='header_container'>
+        <img className='logo' src={logo} alt='logo'/>
+        <nav className='navbar'>
+            <Link to="/" className="nav-header">Accueil</Link>
+            <Link to="/about" className="nav-header">A propos</Link>
+        </nav>
+    </header>
+  )
+}
+
+ 
