ec218c6338f80f12e471ec805ac8e77526459aaf
unknown <d.news@hotmail.fr>
2022-11-22 14:16:43 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 import "./App.css";
-import Header from "./components/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+
+      </Routes>
     </div>
   );
 }
